ef015b22daf9193aa7b2bf1a7f1eaed9736fbe06
Iwanezuk <iwanezuk@hotmail.com>
2025-11-11 17:14:18 -0300

    Update Home.jsx

--- petshop/src/pages/Home.jsx
+++ petshop/src/pages/Home.jsx
@@ -1,10 +1,10 @@
 const Home = () => {
     return(
         <main>
-            <div>
-                <h2>Pet notícias</h2>
+            <div className="container">
+                <h2 className="titulo-pagina">Pet notícias</h2>
             </div>
-            <section>
+            <section className="container flex flex--centro">
                 <article>
                     <h2>Banho no cão</h2>
                     <p>Banhos regulares mantém os pelos e pele de seu cachorro limpa, renovada e saudável. O processo de ensaboar e escovar remove resíduos de pelos e peles que já estão mortas, providenciando um maior respiro da pele de seu cachorro</p>
