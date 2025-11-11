b1537139e327ad356a707a818833ec18a5b4a241
Iwanezuk <iwanezuk@hotmail.com>
2025-11-11 16:33:56 -0300

    Update App.jsx

--- petshop/src/App.jsx
+++ petshop/src/App.jsx
@@ -1,7 +1,8 @@
-import './App.css'
+// import './App.css'
 import Home from './pages/Home'
 // Importe o componente 'Home' pelo nome
 import Sobre from './pages/Sobre'
+import './assets/css/base/base.css'
 
 function App() {
   return (
