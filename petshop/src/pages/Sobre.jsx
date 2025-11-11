232085ff39e3fd88dcdd17629aa5bdd47692f819
Iwanezuk <iwanezuk@hotmail.com>
2025-11-11 17:14:23 -0300

    Update Sobre.jsx

--- petshop/src/pages/Sobre.jsx
+++ petshop/src/pages/Sobre.jsx
@@ -1,10 +1,10 @@
 const Sobre = () => {
     return(
         <main>
-            <div>
-                <h2>Sobre Meu amigo cão</h2>
+            <div className="container">
+                <h2 className="titulo-pagina">Sobre Meu amigo cão</h2>
             </div>
-        <section>
+        <section className="container flex flex--centro">
             <article>
                 Na Meu amigo cão, o nosso sucesso como uma organização é conduzida pelo cuidado que temos com nossos animais.!!!
             </article>
