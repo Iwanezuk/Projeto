555a7cad771d4c5073682e657e024f78dfff6352
Iwanezuk <iwanezuk@hotmail.com>
2025-11-12 13:51:38 -0300

    Create ListaPost.jsx

---
+++ petshop/src/components/ListaPost.jsx
@@ -0,0 +1,34 @@
+import { busca } from "../api.jsx";
+import { useState } from "react";
+
+const ListaPost = ({ url }) => {
+    const [posts, setPosts] = useState([])
+    return (
+        <section className="posts container">
+            {
+                posts.map((posts) =>
+                    <section className="posts container">
+                        {
+                            posts.map((post) =>
+                                <Link className={`cartao-post cartao-post--${post.categoria}`} to={`/posts/${post.id}`} >
+                                    <article key={post.id}>
+                                        <h3 className="cartao-post__titulo">
+                                            {post.title}
+                                        </h3>
+                                        <p className="cartao-post__meta">
+                                            {post.metadescription}
+                                        </p>
+                                    </article>
+                                </Link>
+                            )
+                        }
+                    </section>
+
+                )
+            }
+
+        </section>
+    )
+}
+
+export default ListaPost
