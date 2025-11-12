cc83ca7e28df57ae370576494016079c9a236bc2
Iwanezuk <iwanezuk@hotmail.com>
2025-11-12 11:26:16 -0300

    Update cabecalho.jsx

--- petshop/src/components/cabecalho.jsx
+++ petshop/src/components/cabecalho.jsx
@@ -3,32 +3,29 @@ import '../assets/css/components/cabecalho.css';
 import imagem from '../assets/img/catiorrinho.svg';
 
 const Cabecalho = () => {
-    return(
+  return (
     <header className="cabecalho container">
       <div className="menu-hamburguinho">
         <span className="menu-hamburguinho__icone"></span>
       </div>
       <div className="cabecalho-container">
-        <a href="/" className="flex flex--centro">
-          <img className="cabecalho__logo" src={imagem} alt="Logo Catiorrinho"/>
+        <Link to="/" className="flex flex--centro">
+          <img className="cabecalho__logo" src={imagem} alt="Logo Catiorrinho" />
           <h1 className="cabecalho__titulo">PetShop</h1>
-        </a>
+        </Link>
       </div>
 
       <nav className="menu-cabecalho">
         <ul className="menu-itens">
-          <li><a href="#" className="menu-item menu-item--entrar">Entrar</a></li>
-          <li><a href="#" className="menu-item">Produtos</a></li>
-          <li><a href="/" className="menu-item">Blog</a></li>
-          <li>
-  <Link to="/sobre" className="menu-item">Sobre</Link>
-              <a href="/sobre" ></a>
-          </li>
+          <li><Link to="#" className="menu-item menu-item--entrar">Entrar</Link></li>
+          <li><Link to="#" className="menu-item">Produtos</Link></li>
+          <li><Link to="/" className="menu-item">Blog</Link></li>
+          <li><Link to="/sobre" className="menu-item">Sobre</Link></li>
         </ul>
       </nav>
       <div className="menu-cabecalho-background"></div>
     </header>
-    )
+  )
 }
 
 export default Cabecalho
