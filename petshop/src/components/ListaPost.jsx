e95ff0b834a21d81dc7a00549d5a5333ef5f9526
Iwanezuk <iwanezuk@hotmail.com>
2025-11-12 14:17:07 -0300

    Update ListaPost.jsx

--- petshop/src/components/ListaPost.jsx
+++ petshop/src/components/ListaPost.jsx
@@ -1,34 +1,34 @@
 import { busca } from "../api.jsx";
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import { Link } from 'react-router-dom'
 
 const ListaPost = ({ url }) => {
+
     const [posts, setPosts] = useState([])
+
+    useEffect(() => {
+        busca(url, setPosts)
+    }, [])
+
     return (
+
         <section className="posts container">
             {
-                posts.map((posts) =>
-                    <section className="posts container">
-                        {
-                            posts.map((post) =>
-                                <Link className={`cartao-post cartao-post--${post.categoria}`} to={`/posts/${post.id}`} >
-                                    <article key={post.id}>
-                                        <h3 className="cartao-post__titulo">
-                                            {post.title}
-                                        </h3>
-                                        <p className="cartao-post__meta">
-                                            {post.metadescription}
-                                        </p>
-                                    </article>
-                                </Link>
-                            )
-                        }
-                    </section>
-
+                posts.map((post) =>
+                    <Link className={`cartao-post cartao-post--${post.categoria}`} to={`/posts/${post.id}`} >
+                        <article key={post.id}>
+                            <h3 className="cartao-post__titulo">
+                                {post.title}
+                            </h3>
+                            <p className="cartao-post__meta">
+                                {post.metadescription}
+                            </p>
+                        </article>
+                    </Link>
                 )
             }
-
         </section>
+
     )
 }
 
