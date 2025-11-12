e199ba7434c9d4615b8ceca5daf88cd5d74873ae
Iwanezuk <iwanezuk@hotmail.com>
2025-11-12 17:13:45 -0300

    Create CategoriaPosts.jsx

---
+++ petshop/src/pages/CategoriaPosts.jsx
@@ -0,0 +1,14 @@
+import { useParams } from "react-router-dom";
+import ListaPost from "../components/ListaPost";
+
+const CategoriaPosts = () => {
+    // Captura o parâmetro :id da URL (que vem da rota pai)
+    const { id } = useParams();
+
+    return(
+        // Render do ListaPost com a URL filtrada pela categoria
+        < ListaPost url={`/posts?categoria=${id}`} />
+    )
+}
+
+export default CategoriaPosts;
