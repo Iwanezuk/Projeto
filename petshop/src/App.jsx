4f96128fec2932b6a513dee2c32f6ac828f2fec8
Iwanezuk <iwanezuk@hotmail.com>
2025-11-12 14:55:46 -0300

    Update App.jsx

--- petshop/src/App.jsx
+++ petshop/src/App.jsx
@@ -1,10 +1,11 @@
-import './App.css';
 import './assets/css/base/base.css';
+import './assets/css/post.css'
 import {BrowserRouter as Router, Routes, Route} from 'react-router-dom';
 import Home from './pages/Home';
 import Sobre from './pages/Sobre';
 import Pagina404 from './pages/404';
-import Cabecalho from './components/cabecalho'
+import Cabecalho from './components/cabecalho';
+import Post from './pages/Post';
 
 function App() {
   return(
@@ -13,6 +14,7 @@ function App() {
       <Routes>
         {/* Use element={Componente /} para renderizar */}
         <Route path='/' element={<Home />} />
+        <Route path='/posts/:id' element={<Post />} />
         <Route path='/sobre' element={<Sobre />} />
         <Route path='*' element={<Pagina404 />} />
       </Routes>
