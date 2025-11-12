1e42a974228007640548d54c0140ba6a1f1e4c93
Iwanezuk <iwanezuk@hotmail.com>
2025-11-12 17:24:34 -0300

    Update Categoria.jsx

--- petshop/src/pages/Categoria.jsx
+++ petshop/src/pages/Categoria.jsx
@@ -1,4 +1,5 @@
-import { useParams } from 'react-router-dom'; 
+import { useEffect, useState } from 'react';
+import { useParams, Link, Outlet } from 'react-router-dom';
 import '../assets/css/blog.css';
 import ListaCategorias from '../components/ListaCategorias';
 import ListaPost from '../components/ListaPost';
@@ -6,21 +7,42 @@ import ListaPost from '../components/ListaPost';
 const Categoria = () => {
     // Captura o 'id' da categoria (ex: "bem-estar", "comportamento")
     const { id } = useParams();
+    const [subcategorias, setSubCategorias] = useState([]);
 
-    return(
+    // Busca as subcategorias da categoria principal
+    useEffect(() => {
+        busca(`/categorias/${id}`, (categoria) => {
+            setSubCategorias(categoria.subcategorias);
+        })
+    }, [id]);
+
+    return (
         <>
             <div className="container">
                 <h2 className="titulo-pagina">Pet Notícias</h2>
             </div>
-            
+
             <ListaCategorias />
-            
-            {/*
-              Renderiza o ListaPost diretamente.
-              Passamos a URL de busca filtrada para a API (JSON Server).
-              Ex: /posts?categoria=bem-estar
+
+            {/* Renderiza a lista de links para as subcategorias */}
+            <ul className="lista-categorias container flex">
+                {
+                    subcategorias.map((subcategoria) => (
+                        <li className={`lista-categorias__categoria lista-categorias__categoria--${id}`} key={subcategoria}>
+                            {/* O Link 'to' agora é relativo à rota atual */}
+                            <Link to={subcategoria}>
+                                {subcategoria}
+                            </Link>
+                        </li>
+                    ))
+                }
+            </ul>
+
+            {/* 
+                O <Outlet> é o "espaço reservado" onde o React Router v6
+                irá renderizar os componentes filhos (index ou :subcategoria) definidos no App.jsx.
             */}
-            <ListaPost url={`/posts?categoria=${id}`} />
+            <Outlet />
         </>
     )
 }
