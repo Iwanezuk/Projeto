c434ef6a464ce90d51b1651378b950f1f892fa09
Iwanezuk <iwanezuk@hotmail.com>
2025-11-12 13:42:51 -0300

    Create api.jsx

---
+++ petshop/src/api.jsx
@@ -0,0 +1,14 @@
+import axios from 'axios';
+
+// 1. exportar a instância da base
+export const api = axios.create({
+    baseURL: 'http://localhost:5000' // Endereço do Servidor Json
+})
+
+// 2. Função de busca genérica assíncrona
+// Recebe a URL (ex: '/posts ou '/categorias) e
+// um callback opcional para o sucesso.
+export const busca = async (url) => {
+    // A busca é feita a partir da instância da API
+    const resposta = await api.get(url)
+}
