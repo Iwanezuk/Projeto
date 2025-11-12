2b89ded5c807a54afb24c83a037cd5e069f98ff5
Iwanezuk <iwanezuk@hotmail.com>
2025-11-12 14:29:39 -0300

    Create Post.jsx

---
+++ petshop/src/pages/Post.jsx
@@ -0,0 +1,16 @@
+import {useState, useEffect } from 'react';
+import { busca } from '../api/api';
+
+const Post = () => { 
+  const[post, setPost] = useState({})
+
+  useEffect(() => {
+    busca(`/posts/${id}`, setPost)
+  }, [id])
+
+  return(
+
+  )
+}
+
+export default Post
