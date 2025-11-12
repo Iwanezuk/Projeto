ab338165a005f765f92ea45774d13ffd8f334274
Iwanezuk <iwanezuk@hotmail.com>
2025-11-12 14:17:03 -0300

    Update api.jsx

--- petshop/src/api.jsx
+++ petshop/src/api.jsx
@@ -8,7 +8,10 @@ export const api = axios.create({
 // 2. Função de busca genérica assíncrona
 // Recebe a URL (ex: '/posts ou '/categorias) e
 // um callback opcional para o sucesso.
-export const busca = async (url) => {
+export const busca = async (url,setDado) => {
     // A busca é feita a partir da instância da API
     const resposta = await api.get(url)
+
+    // Atualiza o estado com os dados recebidos
+    setDado(resposta.data)
 }
