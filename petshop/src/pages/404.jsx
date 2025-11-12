246b1ab4aff5a60e2f82231d1252a90e223bbe50
Iwanezuk <iwanezuk@hotmail.com>
2025-11-12 10:06:24 -0300

    Create 404.jsx

---
+++ petshop/src/pages/404.jsx
@@ -0,0 +1,17 @@
+import '../assets/css/404.css'
+
+const Pagina404 = () => {
+    return(
+        <main className="container flex flex--centro flex--coluna">
+            <img className="catiorrinho-imagem" src="" alt="Ilustração Catiorinho" />
+            <p className="naoencontrado-texto">
+                Au, au, foi mau!
+            </p>
+            <p className="naoencontrado-texto">
+                Página não encontrada!
+            </p>
+        </main>
+    )
+}
+
+export default Pagina404
