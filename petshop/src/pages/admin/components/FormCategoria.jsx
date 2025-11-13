89cbe472f4c81b859b504c76941e23ffa607d7c1
Iwanezuk <iwanezuk@hotmail.com>
2025-11-13 10:55:32 -0300

    Update FormCategoria.jsx

--- petshop/src/pages/admin/components/FormCategoria.jsx
+++ petshop/src/pages/admin/components/FormCategoria.jsx
@@ -1,13 +1,32 @@
 import { Button, TextField } from "@mui/material";
+import { useState } from "react";
+import { useNavigate } from "react-router-dom";
+import { api } from "../../../api";
 
 const FormCategoria = () => {
-    
+        const navigate = useNavigate();
+    const [nomeCategoria, setNomeCategoria] = useState('');
+
+    const CadCategoria = (evento) => {
+        evento.preventDefault();
+
+        // Lógica para POST na API
+        api.post(`/categorias`, {
+            id: nomeCategoria, // (JSON-Server usa 'id', mas o ideal seria a API gerar o ID)
+            nome: nomeCategoria,
+            subcategorias: []
+        })
+            .then(() => {
+                alert("Cadastro realizado com Sucesso!");
+                navigate('/admin'); // ATUALIZAÇÃO: Navega para /admin
+            });
+    }
     return ( 
         <main className="container flex flex--centro">
             <article className="cartao post">
                 <h2 className="titulo-pagina">Cadastro de Categorias</h2>
                 <br />
-                <form >
+                <form onSubmit={CadCategoria} >
                     <TextField
                         label="Categoria"
                         variant="filled"
