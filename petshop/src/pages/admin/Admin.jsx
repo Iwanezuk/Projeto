0423677b9320cf7d57bfb8c2094e77c85408367a
Iwanezuk <iwanezuk@hotmail.com>
2025-11-13 09:28:50 -0300

    Create Admin.jsx

---
+++ petshop/src/pages/admin/Admin.jsx
@@ -0,0 +1,11 @@
+const Admin = () => {
+    return(
+        <main>
+            <div className="container">
+                <h2 className="titulo-pagina">Administração</h2>
+            </div>
+        </main> 
+    )
+}
+
+export default Admin
