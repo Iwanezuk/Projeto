6ea9d420b89c28a7aa00f37c72d7b6881799c2b2
Iwanezuk <iwanezuk@hotmail.com>
2025-11-13 09:48:03 -0300

    Update ListaCatAdmin.jsx

--- petshop/src/pages/admin/components/ListaCatAdmin.jsx
+++ petshop/src/pages/admin/components/ListaCatAdmin.jsx
@@ -5,14 +5,43 @@ import "../components/tabela.css";
 
 const ListaCatAdmin = () => {
     const [categorias, setCategorias] = useState([])
-    
+
     useEffect(() => {
         busca(`/categorias`, setCategorias)
     }, [])
 
     return (
-        // O return será construído no próximo passo
-        <></>
+        <section className="container">
+            <table className="tabela">
+                <thead>
+                    <tr>
+                        <th className="tabela__coluna--g">Categoria</th>
+                        <th colSpan="3" className="tabela__coluna--p tabela__alinhamento--direita">
+                            <Link to="/admin/NovaCategoria">
+                                Nova Categoria
+                            </Link>
+                        </th>
+                    </tr>
+                </thead>
+                <tbody>
+                    {
+                        categorias.map((categoria) => (
+                            <tr key={categoria.id}>
+                                <td className="tabela__coluna--m">
+                                    <Link to={`/categoria/${categoria.id}`}>{categoria.nome}</Link>
+                                </td>
+                                <td colSpan="2" className="tabela__coluna--p tabela__alinhamento--direita">
+                                    {/* Botão EDITAR */}
+
+                                    {/* Botão EXCLUIR */}
+
+                                </td>
+                            </tr>
+                        ))
+                    }
+                </tbody>
+            </table>
+        </section>
     )
 }
 
