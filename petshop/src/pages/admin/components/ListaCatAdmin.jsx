f4eac01fe677b7b4fcbf548b27bee68ce2043b21
Iwanezuk <iwanezuk@hotmail.com>
2025-11-13 10:15:07 -0300

    Update ListaCatAdmin.jsx

--- petshop/src/pages/admin/components/ListaCatAdmin.jsx
+++ petshop/src/pages/admin/components/ListaCatAdmin.jsx
@@ -2,6 +2,7 @@ import { useEffect, useState } from "react";
 import { busca } from "../../../api";
 import { Link } from "react-router-dom";
 import "../components/tabela.css";
+import { Button } from "@mui/material";
 
 const ListaCatAdmin = () => {
     const [categorias, setCategorias] = useState([])
@@ -18,7 +19,14 @@ const ListaCatAdmin = () => {
                         <th className="tabela__coluna--g">Categoria</th>
                         <th colSpan="3" className="tabela__coluna--p tabela__alinhamento--direita">
                             <Link to="/admin/NovaCategoria">
-                                Nova Categoria
+                                <Button
+                                    variant="contained"
+                                    fullWidth
+                                    sx={{ marginTop: 1 }}
+                                // usa o sistem de estilização Sx
+                                >
+                                    Nova Categoria
+                                </Button>
                             </Link>
                         </th>
                     </tr>
@@ -28,11 +36,30 @@ const ListaCatAdmin = () => {
                         categorias.map((categoria) => (
                             <tr key={categoria.id}>
                                 <td className="tabela__coluna--m">
-                                    <Link to={`/categoria/${categoria.id}`}>{categoria.nome}</Link>
+                                    <Link
+                                        to={`/categoria/${categoria.id}`}
+                                    >
+                                        {categoria.nome}
+                                    </Link>
                                 </td>
-                                <td colSpan="2" className="tabela__coluna--p tabela__alinhamento--direita">
+                                <td
+                                    colSpan="2"
+                                    className="tabela__coluna--p tabela__alinhamento--direita"
+                                >
                                     {/* Botão EDITAR */}
-
+                                    <Link
+                                        to="/admin"
+                                        style={{ textDecoration: 'none' }}
+                                    >
+                                        <Button
+                                            type="button"
+                                            variant="contained"
+                                            color="warning"
+                                            // Cor MUI para Alerta/Atenção
+                                        >
+                                            Editar
+                                        </Button>
+                                    </Link>
                                     {/* Botão EXCLUIR */}
 
                                 </td>
